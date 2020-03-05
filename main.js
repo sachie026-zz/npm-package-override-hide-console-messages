3b7ad1049cef195f7998088daf9df2cc45cd63fa
Sachin Jadhav <jadhavsachin174@gmail.com>
2020-03-05 21:12:54 +0530

    Add npm package structure

---
+++ main.js
@@ -0,0 +1,1 @@
+function initHideConsole(n=[]){var o,i=(o=window.console,{log:function(i){n.includes("log")||o.log(i)},info:function(i){n.includes("info")||o.info(i)},warn:function(i){n.includes("warn")||o.warn(i)},error:function(i){n.includes("error")||o.error(i)}});window.console=i}module.exports=initHideConsole;
